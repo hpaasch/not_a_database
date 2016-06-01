bd1abc2a5b88620c70e74c988c7361bd7b2866b5
hpaasch <hepaasch@gmail.com>
2016-06-01 19:08:01 -0400

    Normal Mode debugged. Some format work needed.

--- not_database.py
+++ not_database.py
@@ -1,6 +1,15 @@
 import csv
 
 
+def activity():
+    action = input("(C)reate new user? (Q)uit? ").lower()  # add modify later.
+    # if choose M, then present login row without username and allow changes. do a series?
+    if action == "c":
+        user_profile()
+    else:
+        login()
+
+
 def login():
     print("This data is top secret, even by Gen. Petraeus standards. Login to continue.")
     while True:
@@ -15,41 +24,36 @@ def login():
                     if row["password"] == login_password:
                         print("Login successful.")
                         print(row)  # make this pretty
+                        activity()
                         return row
             else:
                 print("Login failed. Try again.")
 
 
-def activity():
-    action = input("(C)reate new user? (Q)uit? ").lower()  # add modify later
-    if action == "c":
-        user_creation()
-    else:
-        login()
+def unique_name_check(username):
+    with open("data_not.csv") as outfile:
+        user_data = csv.DictReader(outfile,
+                                   fieldnames=["username", "password", "full_name", "dog_count"])
+        for row in user_data:
+            if row["username"].lower() == username.lower():
+                print("That username is taken. Try another.")
+                user_profile()
 
 
-def user_creation():
-    while True:
-        username = input("Type in a new username: ")
-        with open("data_not.csv") as outfile:
-            user_data = csv.DictReader(outfile,
-                                       fieldnames=["username", "password", "full_name", "dog_count"])
-            for row in user_data:
-                if row["username"].lower() == username.lower():
-                    print("That username is taken. Try another.")
-            else:
-                return username
+def user_profile():
+    username = input("Type in a new username: ")
+    unique_name_check(username)
     password = input("Set a password: ")
     full_name = input("Full name: ")
     dog_count = int(input("How many dogs in the home? "))
 
-    profile = "{},{},{},{}".format(
+    profile = "{},{},{},{}\n".format(
         username, password, full_name, dog_count)
 
     with open("data_not.csv", "a") as infile:
         infile.write(profile)
 
-    print(profile)
+    activity()
     return profile
 
 
@@ -57,9 +61,4 @@ def modify_user():
     pass
 
 
-# login()
-user_creation()
-# login()
-# activity()
-
-
+login()
