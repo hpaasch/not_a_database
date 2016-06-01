6e056ee255e52cfd2c5c25c2214da228afa0bff5
hpaasch <hepaasch@gmail.com>
2016-06-01 17:29:05 -0400

    Unique username underway.

--- not_database.py
+++ not_database.py
@@ -1,51 +1,65 @@
 import csv
 
 
-def user_creation():
-    username = input("Type in a username: ")
-    password = input("Set a password: ")
-    first_name = input("First name: ").lower()
-    last_name = input("Last name: ").lower()
-    dog_count = int(input("How many dogs in the home? "))
-    boat_count = int(input("How many boats are at the house? "))
-
-    profile = "{},{},{},{},{},{}".format(
-        username, password, first_name, last_name, dog_count, boat_count)
-
-    with open("data_not.csv", "a") as infile:
-        infile.write(profile)
-
-    return profile
-
-
-def verify_login():
+def login():
+    print("This data is top secret, even by Gen. Petraeus standards. Login to continue.")
     while True:
         login_name = input("Username: ")
         login_password = input("Password: ")
 
         with open("data_not.csv") as outfile:
-            user_data = csv.DictReader(outfile, fieldnames=["username", "password"])
+            user_data = csv.DictReader(outfile,
+                                       fieldnames=["username", "password", "full_name", "dog_count"])
             for row in user_data:
                 if row["username"] == login_name:
                     if row["password"] == login_password:
                         print("Login successful.")
-                        return False
-                    else:
-                        print("Login failed. Try again.")
-                else:
-                    print("Login failed. Try again.")
+                        print(row)  # make this pretty
+                        return row
+            else:
+                print("Login failed. Try again.")
 
 
-    # return login_status
+def activity():
+    action = input("(C)reate new user? (Q)uit? ").lower()  # add modify later
+    if action == "c":
+        user_creation()
+    else:
+        login()
+
+
+def user_creation():
+    while True:
+        username = input("Type in a new username: ")
+        with open("data_not.csv") as outfile:
+            user_data = csv.DictReader(outfile,
+                                       fieldnames=["username", "password", "full_name", "dog_count"])
+            for row in user_data:
+                if row["username"].lower() == username.lower():
+                    print("That username is taken. Try another.")
+            else:
+                return username
+    password = input("Set a password: ")
+    full_name = input("Full name: ")
+    dog_count = int(input("How many dogs in the home? "))
+
+    profile = "{},{},{},{}".format(
+        username, password, full_name, dog_count)
+
+    with open("data_not.csv", "a") as infile:
+        infile.write(profile)
+
+    print(profile)
+    return profile
 
 
 def modify_user():
     pass
 
 
-def logout():
-    pass
+# login()
+user_creation()
+# login()
+# activity()
 
 
-# user_inputs()
-verify_login()
